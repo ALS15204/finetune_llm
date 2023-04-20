78e1b830d4112faac2bcf25ffb3b12a9c41c7e3a
Ronin <ronin@iris.ai>
2023-04-20 18:28:17 +0200

    Update README.md and data path

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# finetune_llm
+# Finetune LLMs
+
+## NER task
+1. At project root, retrieve data with ```git clone https://github.com/leslie-huang/UN-named-entity-recognition```
+2. Run `python ner/finetune_lm_ner.py` to finetune a model
--- ner/finetune_lm_ner.py
+++ ner/finetune_lm_ner.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from transformers import TrainingArguments
 
 from ner.dataset import (
@@ -6,41 +8,45 @@ from ner.dataset import (
 )
 from ner.model import BaseNerModel
 
-task = "ner"
-model_checkpoint = "distilbert-base-uncased"
-# model_checkpoint = "gpt2"
-batch_size = 16
-
-train_dataset, test_dataset = get_un_token_dataset(
-    "/home/ronin/Projects/Google_Practice/UN-named-entity-recognition/tagged-training/",
-    "/home/ronin/Projects/Google_Practice/UN-named-entity-recognition/tagged-test/",
-)
+TASK = "ner"
+REPO_ROOT = Path(__file__).parent.parent
 
 
-args = TrainingArguments(
-    f"test-{task}",
-    evaluation_strategy="epoch",
-    learning_rate=1e-4,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    num_train_epochs=3,
-    weight_decay=1e-5,
-)
+if __name__ == "__main__":
+    model_checkpoint = "distilbert-base-uncased"
+    # model_checkpoint = "gpt2"
+    batch_size = 16
 
-model = BaseNerModel(model_checkpoint=model_checkpoint)
-train_tokenized_datasets = train_dataset.map(
-    tokenize_and_align_labels,
-    batched=True,
-    fn_kwargs={"tokenizer": model.tokenizer, "task": task},
-)
-test_tokenized_datasets = test_dataset.map(
-    tokenize_and_align_labels,
-    batched=True,
-    fn_kwargs={"tokenizer": model.tokenizer, "task": task},
-)
-model.train(
-    args=args,
-    train_dataset=train_tokenized_datasets,
-    test_dataset=test_tokenized_datasets,
-)
-model.save("un-ner-bert.model")
+    train_dataset, test_dataset = get_un_token_dataset(
+        REPO_ROOT / "UN-named-entity-recognition/tagged-training/",
+        REPO_ROOT / "UN-named-entity-recognition/tagged-test/",
+    )
+
+
+    args = TrainingArguments(
+        f"test-{TASK}",
+        evaluation_strategy="epoch",
+        learning_rate=1e-4,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=3,
+        weight_decay=1e-5,
+    )
+
+    model = BaseNerModel(model_checkpoint=model_checkpoint)
+    train_tokenized_datasets = train_dataset.map(
+        tokenize_and_align_labels,
+        batched=True,
+        fn_kwargs={"tokenizer": model.tokenizer, "task": TASK},
+    )
+    test_tokenized_datasets = test_dataset.map(
+        tokenize_and_align_labels,
+        batched=True,
+        fn_kwargs={"tokenizer": model.tokenizer, "task": TASK},
+    )
+    model.train(
+        args=args,
+        train_dataset=train_tokenized_datasets,
+        test_dataset=test_tokenized_datasets,
+    )
+    model.save("un-ner-bert.model")
